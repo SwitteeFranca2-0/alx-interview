4d29f64b62cd840176782aa4d2289cf750435ea1
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-07-21 17:16:52 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -16,11 +16,15 @@ def print_file(dic, file_size):
         if i != 0:
             print("{}: {}".format(c, i))
 
+
 if __name__ == "__main__":
     try:
         for line in sys.stdin:
             i += 1
-            file_size += int(line.split()[-1])
+            try:
+                file_size += int(line.split()[-1])
+            except Exception:
+                continue
             code = line.split()[-2]
             if code.isnumeric():
                 if int(code) in status_code.keys():
@@ -28,7 +32,7 @@ if __name__ == "__main__":
             if i == 10:
                 print_file(status_code, file_size)
                 i = 0
-            print_file(status_code, file_size)
+        print_file(status_code, file_size)
     except KeyboardInterrupt as e:
         print_file(status_code, file_size)
         raise
