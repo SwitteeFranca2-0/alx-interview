9a29209ec363fdc77b3064926ea9efc1cafc8f13
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-07-21 01:58:30 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,28 +3,32 @@
 
 import sys
 
-status_code = {}
+status_code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0,
+               404: 0, 405: 0, 500: 0}
 file_size = 0
 
 i = 0
+
+
+def print_file(dic, file_size):
+    print("File Size: {}".format(file_size))
+    stat_code = dict(sorted(dic.items()))
+    for c, i in stat_code.items():
+        if i != 0:
+            print("{}: {}".format(c, i))
+
+
 try:
     for line in sys.stdin:
         i += 1
         file_size += int(line.split()[-1])
         code = line.split()[-2]
-        if code not in status_code.keys():
-            status_code[code] = 1
-        else:
-            status_code[code] += 1
+        if code.isnumeric():
+            if int(code) in status_code.keys():
+                status_code[int(code)] += 1
         if i == 10:
-            print("File Size: {}".format(file_size))
-            stat_code = dict(sorted(status_code.items()))
-            for c, i in stat_code.items():
-                print("{}: {}".format(c, i))
+            print_file(status_code, file_size)
             i = 0
 except KeyboardInterrupt as e:
-    print("File Size: {}".format(file_size))
-    stat_code = dict(sorted(status_code.items()))
-    for c, i in stat_code.items():
-        print("{}: {}".format(c, i))
-    raise e
+    print_file(status_code, file_size)
+    raise
