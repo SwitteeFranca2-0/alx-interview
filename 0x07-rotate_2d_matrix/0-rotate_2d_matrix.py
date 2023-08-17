fa9904c25ce490cef5b544e1948f01455cd92223
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-08-17 21:10:39 +0100

    rotating matricex

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Rotating matrices given"""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotating 2d matrices"""
+    row = len(matrix)
+    column = len(matrix[0])
+    columns = []
+    for i in range(column):
+        col = []
+        for r in matrix:
+            col.append(r[i])
+        col.reverse()
+        columns.append(col)
+    matrix.clear()
+    for col in columns:
+        matrix.append(col)
