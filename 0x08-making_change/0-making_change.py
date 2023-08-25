03b9406067470057fd7439a323a8eebcc75f0426
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-08-25 03:51:36 +0100

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Make changes function"""
+
+
+def makeChange(coins, total):
+    """Finding the least amount of coins needed to make a sum"""
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
