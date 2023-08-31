d505128cd9ce314adbd7aa5f9f54e1f292350601
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-08-31 21:38:51 +0100

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3 
+
+def island_perimeter(grid):
+    """This function finds the area of the island perimeter"""
+    height = len(grid)
+    width = len(grid[0])
+    length = 0
+    breadth = 0
+    for row in grid:
+        if 1 not in row and length != 0:
+            break
+        if 1 in row:
+            length += 1
+            if row.count(1) > breadth:
+                breadth = row.count(1)
+    return 2*(length + breadth)
+    
+
